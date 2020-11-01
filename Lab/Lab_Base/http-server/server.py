630c75f6d99ab4fb144feb544dc129eb387e7524
Fahmida Haque <71458781+Fahmida-Haque@users.noreply.github.com>
2020-10-31 20:40:30 -0400

    Update server.py

--- Lab/Lab_Base/http-server/server.py
+++ Lab/Lab_Base/http-server/server.py
@@ -1,10 +1,40 @@
 from flask import Flask
+from flask import Request
+from datetime import datetime
+import time
+from elasticsearch import Elasticsearch
+
+local = False
+
+if local:
+    esHost = 'localhost'
+    httpHost = 'localhost'
+else:
+    esHost = "es01"
+    httpHost = 'http'
+
+es = Elasticsearch(host=esHost)
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Demo Flask & Docker application is up and running!"
+@app.route("/process_data", methods=["POST"])
+def process_data():
+    data = Request.get_json(force=True)
+    
+    type = data['type'].format(str())
+    secret = data['data]'].format(str())
+    client_time = data['client_time'].format(datetime(), time)
+    device = data['device'].format(str())
+    client_ID = data['client_ID'].format(str())
+    
+    
+    return '''
+           The type is: {}
+           The secret is: {}
+           The client time is: {}
+           The device is: {}
+           The client ID is: {} '''
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run()
+        
