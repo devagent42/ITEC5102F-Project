6c2f67b3645b0443e141b14a10bb36e6f541a98f
Georges A <16962003+devagent42@users.noreply.github.com>
2020-10-31 20:56:28 -0400

    Fixed.

--- Lab/Lab_Base/http-server/server.py
+++ Lab/Lab_Base/http-server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import Request
+from flask import request
 from datetime import datetime
+import json
 import time
 from elasticsearch import Elasticsearch
 
@@ -17,24 +18,29 @@ es = Elasticsearch(host=esHost)
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return "Demo Flask & Docker application is up and running!"
+
 @app.route("/process_data", methods=["POST"])
 def process_data():
-    data = Request.get_json(force=True)
-    
-    type = data['type'].format(str())
-    secret = data['data]'].format(str())
-    client_time = data['client_time'].format(datetime(), time)
-    device = data['device'].format(str())
-    client_ID = data['client_ID'].format(str())
-    
-    
+    data = request.data#Request.get_json(force=True)
+    data = data.decode("utf-8")
+    data = json.loads(data)
+    print (data)
+    #type = data['type'].format(str())
+    #secret = data['data]'].format(str())
+    #client_time = data['client_time'].format(datetime(), time)
+    #device = data['device'].format(str())
+    #client_ID = data['client_ID'].format(str())
+
+
     return '''
            The type is: {}
            The secret is: {}
            The client time is: {}
            The device is: {}
-           The client ID is: {} '''
-
-if __name__ == "__main__":
-    app.run()
-        
+           The client ID is: {}
+           '''
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
