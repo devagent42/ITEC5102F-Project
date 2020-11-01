1e90afa0fa1c1a89dd253fcac362143019c4753a
Georges A <16962003+devagent42@users.noreply.github.com>
2020-10-31 20:57:40 -0400

    Changed return.

--- Lab/Lab_Base/http-server/server.py
+++ Lab/Lab_Base/http-server/server.py
@@ -35,12 +35,7 @@ def process_data():
     #client_ID = data['client_ID'].format(str())
 
 
-    return '''
-           The type is: {}
-           The secret is: {}
-           The client time is: {}
-           The device is: {}
-           The client ID is: {}
-           '''
+    return "Well received!"
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
