fdba02ea086d5de15f5ef33bc2404ed214a22f98
Georges A <16962003+devagent42@users.noreply.github.com>
2020-11-06 19:19:56 -0500

    Removed STunnel from base. Created new folder.

---
+++ Lab/Lab sTunnel/mqtt-client/client.py
@@ -0,0 +1,35 @@
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json, uuid, random
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import time
+ID = uuid.uuid4()
+ID = str(ID).replace('-','')
+
+local = False
+
+if local:
+    esHost = 'localhost'
+    mqttHost = 'localhost'
+else:
+    esHost = "es01"
+    mqttHost = 'mqtt'
+
+es = Elasticsearch(host=esHost)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect(mqttHost, 1883, 60)
+while True:
+    now = datetime.utcnow()
+    secret = {"type":"secret","secret":str(uuid.uuid4()),"client_time":now.timestamp(),"device":"mqtt_client","client_ID":str(ID)}
+    client.publish("secret/"+str(ID),json.dumps(secret))
+    #print (json.dumps(secret))
+    secret = {"type":"secret","secret":str(uuid.uuid4()),"client_time":now,"device":"mqtt_client","client_ID":str(ID)}
+    es.index(index="client-data", doc_type="_doc", body=secret)
+    time.sleep(2)
---
+++ Lab/Lab sTunnel/mqtt-server/server.py
@@ -0,0 +1,36 @@
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json, uuid, random
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import time
+
+local = False
+
+if local:
+    esHost = 'localhost'
+    mqttHost = 'localhost'
+else:
+    esHost = "es01"
+    mqttHost = 'mqtt'
+
+es = Elasticsearch(host=esHost)
+channelSubs="secret/#"
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(channelSubs)
+
+def on_message(client, userdata, msg):
+    try:
+        msg = json.loads(msg.payload)
+        msg["client_time"] = datetime.fromtimestamp(msg["client_time"])
+        body = {"server_time":datetime.utcnow(),"device":"mqtt_server","message":msg}
+        es.index(index="server-data", doc_type="_doc", body=body)
+    except:
+        pass
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(mqttHost, 1883, 60)
+client.loop_forever()
