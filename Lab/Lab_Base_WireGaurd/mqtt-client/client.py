f421ab9f69360cf01c34fdcfbfe466b8bb61f120
JeffreyAJBailey <jeffajbailey@gmail.com>
2020-11-16 18:54:18 -0500

    added wiregaurd lab folder

---
+++ Lab/Lab_Base_WireGaurd/mqtt-client/client.py
@@ -0,0 +1,36 @@
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json, uuid, random
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import time
+ID = uuid.uuid4()
+ID = str(ID).replace('-','')
+
+local = False
+
+if local:
+    esHost = 'localhost'
+    mqttHost = 'localhost'
+else:
+    esHost = "es01"
+    mqttHost = 'mqtt'
+
+es = Elasticsearch(host=esHost)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect(mqttHost, 1883, 60)
+while True:
+    now = datetime.utcnow()
+    secret_passwd = str(uuid.uuid4())
+    secret = {"type":"secret","secret":secret_passwd,"client_time":now.timestamp(),"device":"mqtt_client","client_ID":str(ID)}
+    client.publish("secret/"+str(ID),json.dumps(secret))
+    secret = {"timestamp":datetime.utcnow(),"type":"secret","secret":secret_passwd,"client_time":now,"device":"mqtt_client","client_ID":str(ID)}
+    print (secret)
+    es.index(index="data", doc_type="_doc", body=secret)
+    time.sleep(2)
