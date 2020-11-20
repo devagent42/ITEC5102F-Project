5ef8e3a8423566c5af450b68d35127eb602fc7fe
Fahmida Haque <71458781+Fahmida-Haque@users.noreply.github.com>
2020-11-20 00:53:03 -0500

    No update yet.

---
+++ Lab/Lab_Base_Shadowsocks/http-client/client.py
@@ -0,0 +1,32 @@
+import requests
+import uuid
+from datetime import datetime
+import time
+from elasticsearch import Elasticsearch
+
+now = datetime.utcnow()
+ID = uuid.uuid4()
+ID = str(ID).replace('-', '')
+
+local = False
+
+if local:
+    esHost = 'localhost'
+    httpHost = 'localhost'
+else:
+    esHost = "es01"
+    httpHost = 'http'
+
+es = Elasticsearch(host=esHost)
+
+while True:
+    secret_passwrd = uuid.uuid4()
+    response = requests.post('http://'+httpHost+':5000/process_data',
+                            json={"type": "secret", "secret": str(secret_passwrd), "client_time": str(now.isoformat()),
+                                  "device": "http_client",
+                                  "client_ID": str(ID)})
+
+    #print("Status code: ", response.status_code)
+    secret = {"timestamp":datetime.utcnow(),"type": "secret", "secret": str(secret_passwrd), "client_time": now, "device": "http_client", "client_ID": str(ID)}
+    es.index(index="data", doc_type="_doc", body=secret)
+    time.sleep(2)
